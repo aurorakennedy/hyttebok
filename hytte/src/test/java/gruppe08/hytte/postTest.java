ddd37d4dca97c1d4058fdba25ff6414d8a80bf43
Aurora Kennedy <aurorakennedy@dhcp-10-22-14-225.wlan.ntnu.no>
2022-09-15 13:04:48 +0200

    Terst tilk post laget

--- hytte/src/test/java/gruppe08/hytte/postTest.java
+++ hytte/src/test/java/gruppe08/hytte/postTest.java
@@ -1,5 +1,38 @@
 package gruppe08.hytte;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class postTest {
+
+    private Post post; 
+
+    @BeforeEach
+    // Lager en post, slik at vi kan teste at post fungerer som den skal 
+    private void creatPost(){
+        post = new Post("Knut", "Hei, dette er en test. Håper den funker.", LocalDate.of(2000, 11, 4)); 
+    }
     
+    @Test
+    public void testConstructer(){
+        // Sjekker om verdiene er det vi forventer 
+        Assertions.assertEquals("Knut", post.getName());
+        Assertions.assertEquals("Hei, dette er en test. Håper den funker.", post.getContent());
+        Assertions.assertEquals(LocalDate.of(2000, 11, 4), post.getDate());
+
+
+        // --- Navn ---
+        //Sjekker at navn ikke kan innholde tall 
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new Post("Klara4", "Heihei", LocalDate.of(2000, 11, 4)); 
+		});
+        //Sjekker at navn ikke kan inneholde special characters 
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new Post("Trym/Aksel", "Tigergutt", LocalDate.of(2000, 11, 4)); 
+		});
+
+    }
 }
