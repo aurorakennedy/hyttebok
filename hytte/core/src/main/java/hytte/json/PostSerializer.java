a25cc7482dc60842fa59e4a43c1ea0dfd1179b22
akselhl <akselhl@stud.ntnu.no>
2022-10-07 11:52:24 +0200

    Created seralizer, fixed module-info

--- hytte/core/src/main/java/hytte/json/PostSerializer.java
+++ hytte/core/src/main/java/hytte/json/PostSerializer.java
@@ -1,11 +1,29 @@
 package hytte.json;
 
-import java.time.format.DateTimeFormatter;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import hytte.core.Post;
 
-public class PostSerializer extends JsonSerializer {
+public class PostSerializer extends JsonSerializer<Post> {
 
-    private static final DateTimeFormatter DATE_FORMATTER = ISOD
-
-
-}
+    /*
+     format:
+     {
+        "date": 00:00:0000,
+        "name": "text",
+        "postText": "text"
+     }
+     */
+  
+    @Override
+    public void serialize(Post post, JsonGenerator jGen, SerializerProvider serializerProvider) throws IOException {
+      jGen.writeStartObject();
+      /*jGen.writeDateField("date", post.getDate());*/
+      jGen.writeStringField("name", post.getName());
+      jGen.writeStringField("postText", post.getContent());
+      jGen.writeEndObject();
+    }
+  }
--- hytte/core/src/main/java/module-info.java
+++ hytte/core/src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module hytte.core {
 
+    requires org.json;
+    requires transitive com.fasterxml.jackson.databind;
+
     exports hytte.core;
+    exports hytte.json;
     //exports hytte.json;
 }
