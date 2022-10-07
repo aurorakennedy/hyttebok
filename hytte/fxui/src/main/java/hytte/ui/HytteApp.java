1bee9145e054e790cedcd5788c8183f5e5b5dc1c
Trym Eidsten <trymme@ntnu.no>
2022-10-07 11:59:11 +0200

    Issue #34 and #35, and renamed hytteApp to HytteApp

--- hytte/fxui/src/main/java/hytte/ui/HytteApp.java
+++ hytte/fxui/src/main/java/hytte/ui/HytteApp.java
@@ -7,7 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class hytteApp extends Application {
+public class HytteApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
