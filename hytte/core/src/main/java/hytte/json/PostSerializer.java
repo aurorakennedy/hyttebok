483d0157926536d27e4fb0d53aeffe0d08b47a84
Klara Wüstenberg <klarawust@gmail.com>
2022-10-07 11:23:16 +0200

    created PostSerializer

---
+++ hytte/core/src/main/java/hytte/json/PostSerializer.java
@@ -0,0 +1,11 @@
+package hytte.json;
+
+import java.time.format.DateTimeFormatter;
+import com.fasterxml.jackson.databind.JsonSerializer;
+
+public class PostSerializer extends JsonSerializer {
+
+    private static final DateTimeFormatter DATE_FORMATTER = ISOD
+
+
+}
