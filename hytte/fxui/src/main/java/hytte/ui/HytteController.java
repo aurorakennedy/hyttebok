4d9cfc3ba3df6154a3fca2f0ba34c952478c124d
Klara Wüstenberg <klarawust@gmail.com>
2022-10-25 12:39:39 +0200

    #51 bug in new window-fxml

--- hytte/fxui/src/main/java/hytte/ui/HytteController.java
+++ hytte/fxui/src/main/java/hytte/ui/HytteController.java
@@ -5,16 +5,49 @@ import java.time.LocalDate;
 
 import hytte.core.Post;
 import hytte.core.PostList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class HytteController {
+
+    //Nytt vindu for å kunne lese innlegg
+    @FXML
+    private Button seePreviousPostsButton; 
+
+    @FXML
+    private ScrollPane scrollPane; //brukes til å sende tidligere innlegg til pane
+
+    @FXML
+    void seePreviousPosts(ActionEvent event){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("previousPosts.fxml")); //lager en ny FXML loader som laster inn innholdet i previousPosts.fxml
+            Parent root1 = (Parent) fxmlLoader.load(); //setter parent/rot til den nye filen previousPosts.html
+            Stage stage = new Stage(); //lager nytt vindu
+            stage.initStyle(StageStyle.TRANSPARENT); //setter vindu-lukke og minimerknappen til transparent
+            stage.setTitle("Posts"); //setter tittelen til selve vinduet til "Posts"
+            stage.setScene(new Scene(root1)); //lager ny scene i roten
+            stage.show(); //viser nye vinduet
+        } catch (Exception e){
+            System.out.println("Can not load new window.");
+        }
+    }
+
+
+
+
 
     private PostList postList = new PostList();
 
@@ -26,9 +59,6 @@ public class HytteController {
 
     @FXML
     private Button saveButton; //importerer FXML-button-feltene
-
-    @FXML
-    private Button previousPostsButton;
 
     @FXML
     private DatePicker datePicker; //importerer FXML-datePicker-feltet
