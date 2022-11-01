3a25c8f5709f11276c8fa11227ba90e8228ea968
Trym Eidsten <trymme@ntnu.no>
2022-11-01 16:31:36 +0100

    Issue #54

--- hytte/fxui/src/main/java/module-info.java
+++ hytte/fxui/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module hytte.ui {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
+    requires java.net.http;
 
     requires transitive hytte.core;
     exports hytte.ui;
