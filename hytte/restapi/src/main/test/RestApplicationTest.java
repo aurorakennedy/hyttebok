4fe577dd021a1684a95177d163bb0424d7ae8cbd
akselhl <akselhl@stud.ntnu.no>
2022-11-15 14:25:40 +0100

    #63 Setup pom for restapi testing and created class for tests

---
+++ hytte/restapi/src/main/test/RestApplicationTest.java
@@ -0,0 +1,23 @@
+package hytte.restapi;
+
+import io.restassured.RestAssured;
+import io.restassured.matcher.RestAssuredMatchers;
+import org.hamcrest.Matchers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class RestApplicationTest {
+    
+    @Test
+    public void moroTest () {
+        try {
+            assertEquals(1, 1);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+}
